7cc583e3a16c7a285ac02da3ffddc3ff7cab2a49
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-06-24 13:54:02 +0300

    fix: move to project root folder

---
+++ controllers/AppController.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/extensions */
+import asyncHandler from 'express-async-handler';
+import redisClient from '../utils/redis.js';
+import dbClient from '../utils/db.js';
+
+export const getStatus = asyncHandler(async (req, res) => {
+  if (redisClient.isAlive() && dbClient.isAlive()) {
+    res.status(200).send('{ "redis": true, "db": true }');
+  }
+});
+
+export const getStats = asyncHandler(async (req, res) => {
+  const users = await dbClient.nbUsers();
+  const files = await dbClient.nbFiles();
+  res.status(200).send(`{ "users": ${users}, "files": ${files} }`);
+});
