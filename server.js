918a277ab4d9290fdd8dcc5274535f3e6a30ee3f
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-06-24 13:51:06 +0300

    feat: Add index router

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-import express from "express";
-import routes 
+/* eslint-disable import/extensions */
+import express from 'express';
+import indexRouter from './routes/index.js';
+
 const ENV = process.env;
 const PORT = ENV.PORT || 5000;
 
 const app = express();
-app.
 app.listen(PORT, () => console.log('Server running on port 5000'));
+app.use('/', indexRouter);
